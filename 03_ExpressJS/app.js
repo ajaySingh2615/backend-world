4b451990d230b8a28a6dfc69f81328250d47730e
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-17 14:13:44 +0530

    modules in nodejs

--- 03_ExpressJS/app.js
+++ 03_ExpressJS/app.js
@@ -3,11 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminData.routes);
---
+++ 04_HelloWorld/hello.js
@@ -0,0 +1,4 @@
+const math = require("./math");
+
+console.log("The value of math is after adding : ", math.add(6, 2));
+console.log("The value of math is after sub : ", math.sub(6, 2));
