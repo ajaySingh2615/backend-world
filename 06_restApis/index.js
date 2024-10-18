97d2c984655a79f55ecb990d5c1443552401c73a
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-18 21:10:31 +0530

    connecting with mongoDB

--- 06_restApis/index.js
+++ 06_restApis/index.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const fs = require("fs");
-const users = require("./MOCK_DATA.json");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 8080;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/demo")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Mongo Error", err));
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    jobTitle: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -16,44 +47,38 @@ app.use((req, res, next) => {
     }
   );
 });
-
 // Routes
 
-app.get("/api/users", (req, res) => {
-  res.setHeader("myName", "Ajay Singh");
-  return res.json(users);
-});
-
-app.get("/users", (req, res) => {
-  const html = `
-    <ul>
-    ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
-    </ul>
-    `;
-
-  res.send(html);
+app.get("/api/users", async (req, res) => {
+  const allDbUsers = await User.find({});
+  return res.json(allDbUsers);
 });
 
 app
   .route("/api/users/:id")
-  .get((req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
+  .get(async (req, res) => {
+    const user = await User.findById(req.params.id);
     return res.json(user);
   })
-  .patch((req, res) => {
-    res.json({ status: "Pending" });
+  .patch(async (req, res) => {
+    await User.findByIdAndUpdate(req.params.id, { lastName: "changed" });
+    res.json({ status: "Success" });
   })
   .delete((req, res) => {
     res.json({ status: "Pending" });
   });
 
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
   const body = req.body;
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "pending" });
+
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.job_title,
   });
+  return res.status(201).json({ msg: "success" });
 });
 
 app.listen(PORT, () => {
