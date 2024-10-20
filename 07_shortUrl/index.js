6b01360c49d946bec00a9647069e5478fe551cc9
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-10-20 19:18:32 +0530

    server side rendering with ejs and nodejs

--- 07_shortUrl/index.js
+++ 07_shortUrl/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const path = require("path");
 const urlRoute = require("./routes/url");
 const { connectToMongoDB } = require("./connection");
 const URL = require("./models/url");
+const staticRoute = require("./routes/staticRouter");
 
 const app = express();
 const PORT = 8080;
@@ -10,11 +12,16 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
   console.log("MongoDb connected")
 );
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
